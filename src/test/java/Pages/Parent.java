9a674fbefd04b3c2ef573a0a2ac198d424070aff
erkankurtaga <erkankurtaga@gmail.com>
2022-09-15 21:28:57 +0300

    CUC-3 First Commit

--- src/test/java/Pages/Parent.java
+++ src/test/java/Pages/Parent.java
@@ -6,9 +6,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.time.Duration;
+import java.util.List;
 
 public class Parent {
 
@@ -27,6 +31,11 @@ public class Parent {
     public void scrollToElement(WebElement element) {
         JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
         js.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public void scrollUp() {
+        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+        js.executeScript("window.scrollTo(0, -250)");
     }
 
     public void clickFunction(WebElement element)
@@ -52,4 +61,37 @@ public class Parent {
         wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("fuse-progress-bar > *"), 0));
     }
 
+    public void setAndWaitAttributeToBe(WebElement element, String attName, String attValue){
+        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+        js.executeScript("arguments[0].setAttribute(arguments[1], arguments[2]);", element, attName, attValue);
+
+        WebDriverWait wait=new WebDriverWait(GWD.driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.domAttributeToBe(element, attName, attValue));
+    }
+
+    public void sendKeyBoard(String key){
+        int keyInt = 0;
+
+        switch (key){
+            case "ESC":
+                keyInt = KeyEvent.VK_ESCAPE;
+                break;
+            case "TAB":
+                keyInt = KeyEvent.VK_TAB;
+                break;
+            case "ENTER":
+                keyInt = KeyEvent.VK_ENTER;
+                break;
+        }
+
+        if(keyInt > 0) {
+            try {
+                Robot robot = new Robot();
+                robot.keyPress(keyInt);
+                robot.keyRelease(keyInt);
+            } catch (AWTException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
